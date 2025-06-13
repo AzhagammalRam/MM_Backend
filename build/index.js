3a6647837c46a9f6e5fd5ec3a12c3c2ae560cf25
AzhagammalRam <azhagammal07@gmail.com>
2025-06-13 17:10:15 +0530

    Delete build directory

--- build/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const createApp_1 = require("./createApp");
-const app = (0, createApp_1.createApp)();
-const PORT = process.env.SERVER_PORT;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
